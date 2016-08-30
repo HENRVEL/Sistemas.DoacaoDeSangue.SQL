94c52a3f2cd287e4d1a1485bee1f3ea885ff9e36
HENRVEL <HENRIQUE@DESKTOP-SI44J2O>
2016-08-29 22:18:42 -0300

    Segurança.

---
+++ 3 - Procedures/Segurança/CriarUsuario.sql
@@ -0,0 +1,40 @@
+USE [BD_DOACAO]
+GO
+
+/**
+Banco: BD_DOACAO
+Date: 29/08/2016	
+**/
+
+ALTER PROCEDURE CriarUsuario
+(
+	 @USUARIO varchar(30),
+     @SENHA varchar(30),
+     @TIPO_USUARIO varchar(3),
+     @ID_DOA int,
+     @ID_UHP int,
+     @ID_LAB int
+)
+AS 
+BEGIN
+	DECLARE @SENHA_AUX varbinary(100) 
+	SET @SENHA_AUX = PWDENCRYPT(@SENHA)
+
+
+	INSERT INTO [dbo].[TB_CD_USUARIO_USA]
+           ([USUARIO]
+           ,[SENHA]
+           ,[TIPO_USUARIO]
+           ,[ID_DOA]
+           ,[ID_UHP]
+           ,[ID_LAB])
+     VALUES
+           (@USUARIO
+           ,@SENHA_AUX
+           ,@TIPO_USUARIO
+           ,@ID_DOA
+           ,@ID_UHP
+           ,@ID_LAB
+		   )
+END 
+GO 
